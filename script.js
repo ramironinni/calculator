44d1cd3307b98715d31150fc8625aa6961a79843
ramironinni <ramironinni@gmail.com>
2021-05-24 16:13:25 -0300

    Create basic functions

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ window.addEventListener("DOMContentLoaded", () => {
         }, 0);
         return result;
     }
+
+    function substract(...numbers) {
+        if (numbers.length < 1) {
+            return 0;
+        }
+        const result = numbers.reduce((accum, item) => {
+            return accum - item;
+        });
+        return result;
+    }
+
+    function multiply(...numbers) {
+        const result = numbers.reduce((accum, item) => {
+            return accum * item;
+        });
+        return result;
+    }
+
+    function divide(...numbers) {
+        const result = numbers.reduce((accum, item) => {
+            return accum / item;
+        });
+        return result;
+    }
 });
